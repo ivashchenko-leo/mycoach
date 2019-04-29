a806cccdefb0664c903cc9fcca4c611dfe8871ae
Leonid Ivashchenko <leonidivashchenko@MacBook-Pro-6.local>
2019-04-29 16:35:47 +0900

    added custom form for user model, email, first_name, last_name are required now

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import CoachProfile, CoachPost, Sport, CoachPhoto
+from django.contrib.auth.models import User
+from .forms import CustomUserAdmin
 
 # Register your models here.
 admin.site.register(CoachPost)
 admin.site.register(CoachProfile)
 admin.site.register(CoachPhoto)
 admin.site.register(Sport)
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import CoachProfile, CoachPost, CoachPhoto
 from rest_framework import serializers
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.admin import UserAdmin
 
 
 class CoachProfileForm(forms.ModelForm):
@@ -30,3 +33,24 @@ class UserSigninSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         pass
+
+
+class CustomUserChangeForm(UserChangeForm):
+    def clean_first_name(self):
+        if self.cleaned_data["first_name"].strip() == '':
+            raise ValidationError("First name is required.")
+        return self.cleaned_data["first_name"]
+
+    def clean_last_name(self):
+        if self.cleaned_data["last_name"].strip() == '':
+            raise ValidationError("Last name is required.")
+        return self.cleaned_data["last_name"]
+
+    def clean_email(self):
+        if self.cleaned_data["email"].strip() == '':
+            raise ValidationError("Email is required.")
+        return self.cleaned_data["email"]
+
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
