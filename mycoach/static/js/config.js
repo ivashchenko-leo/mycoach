442be0581009caa268127a6f78bbbf6c6ae05b6d
Leonid Ivashchenko <leonidivashchenko@MacBook-Pro-6.local>
2019-07-08 15:46:27 +0900

    added frontend

---
+++ mycoach/static/js/config.js
@@ -0,0 +1,6 @@
+var SERVER_ADDRESS = "http://localhost:8080";
+
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+import SimpleImage from '@editorjs/simple-image'
